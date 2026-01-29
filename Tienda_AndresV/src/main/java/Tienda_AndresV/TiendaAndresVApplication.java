a838f194b142d18abb2abdf511060e504140b3f1
Andres-2707 <andresjvj652@gmail.com>
2026-01-28 19:57:31 -0600

    Rec
    Rec

---
+++ Tienda_AndresV/src/main/java/Tienda_AndresV/TiendaAndresVApplication.java
@@ -0,0 +1,13 @@
+package Tienda_AndresV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAndresVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAndresVApplication.class, args);
+	}
+
+}
---
+++ Tienda_AndresV/src/test/java/Tienda_AndresV/TiendaAndresVApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_AndresV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAndresVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
